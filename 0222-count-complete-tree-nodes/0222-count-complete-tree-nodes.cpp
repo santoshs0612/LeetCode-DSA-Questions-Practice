6c309b9a92cdd1bd68e13c026f15788b80d1cec3
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-01-15 15:14:37 +0530

    Time: 0 ms (100%), Space: 31.1 MB (78.89%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -20,14 +20,52 @@ private:
         inorder(root->left,count);
         inorder(root->right,count);
     }
+
+    int solverRec(TreeNode* root){
+
+        if(root==NULL){
+            return 0;
+        }
+
+        int lh = leftTreeHeight(root);
+        int rh = rightTreeHeight(root);
+
+        if(lh==rh){
+            return (1<<lh)-1;
+        }
+
+        return 1+solverRec(root->left)+ solverRec(root->right);
+    }
+
+    int leftTreeHeight(TreeNode* root){
+
+        int count =0;
+
+        while(root){
+            count++;
+            root= root->left;
+        }
+        return count;
+    }
+    int rightTreeHeight(TreeNode* root){
+
+
+        int count =0;
+
+        while(root){
+            count++;
+            root= root->right;
+        }
+        return count;
+    }
 public:
     int countNodes(TreeNode* root) {
 
-        int count=0;
-        inorder(root,count);
-        return count;
+        // int count=0;
+        // inorder(root,count);
+        // return count;
 
-
+        return solverRec(root);
         
     }
 };
