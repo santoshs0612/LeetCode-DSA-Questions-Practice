37ab00eec385a995c0def9051b7bf416a560e6f3
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-01-16 11:22:47 +0530

    Time: 0 ms (100%), Space: 10.7 MB (77.47%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -17,11 +17,44 @@ private:
         ans.push_back(root->val);
         solverRec(root->right,ans);
     }
+    void solverMorrishInorder(TreeNode* root, vector<int> &ans){
+
+        while(root){
+            if(root->left==NULL){
+                ans.push_back(root->val);
+                root = root->right;
+            }
+            else{
+
+                TreeNode* prev = root->left;
+
+                while(prev->right && prev->right!=root){
+                    prev =prev->right;
+                }
+
+                if(prev->right==NULL){
+                    prev->right = root;
+                    root = root->left;
+                }else{
+                    prev->right = NULL;
+                    ans.push_back(root->val);
+                    root = root->right;
+                }
+            }
+
+        }
+    }
 public:
     vector<int> inorderTraversal(TreeNode* root) {
+        // recurssive 
+        // vector<int> inorder;
+        // solverRec(root,inorder);
+        // return inorder;
+
+        // morrish traversal with o(n) and space of O(1)
+
         vector<int> inorder;
-        solverRec(root,inorder);
-        return inorder;
-        
+        solverMorrishInorder(root,inorder);
+        return inorder;        
     }
 };
