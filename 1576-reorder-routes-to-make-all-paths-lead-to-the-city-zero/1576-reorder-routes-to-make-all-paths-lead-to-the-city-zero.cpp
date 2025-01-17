0bf085bdc9da48e91fac25825ec4b44e988d755b
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-01-17 15:48:54 +0530

    Time: 91 ms (79.01%), Space: 107.1 MB (65.48%) - LeetHub

--- 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1576-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -1,40 +1,68 @@
 class Solution {
+private:
+    void dfsSolver(int node, vector<vector<pair<int,int>>> &adjList,vector<int> &visited){
+
+        visited[node]=1;
+        for(auto it:adjList[node]){
+            int nbNode = it.first;
+            int val=it.second;
+            if(visited[nbNode]==0){
+                count+=val;
+                dfsSolver(nbNode,adjList,visited);
+            }
+              
+        }
+    }
 public:
+    int count =0 ; 
     int minReorder(int n, vector<vector<int>>& connections) {
-        int count=0;
-        int m = connections.size();
+        // int count=0;
+        // int m = connections.size();
+        // vector<int> visited(n,0);
+        // // creating a adj List 
+        // vector<vector<int>> adjList(n);
+        // vector<vector<int>> revAdjList(n);
+        // for(auto connection:connections){
+        //     adjList[connection[0]].push_back(connection[1]);
+        //     revAdjList[connection[1]].push_back(connection[0]);
+        // }
+
+        // queue<int> q;
+        // q.push(0);
+        // visited[0]=1;
+
+        // while(!q.empty()){
+
+        //     int node = q.front();
+        //     q.pop();
+
+        //     for(auto nb:adjList[node]){
+        //         if(!visited[nb]){
+        //             count++;
+        //             q.push(nb);
+        //             visited[nb]=1;
+        //         }
+        //     }
+        //     for(auto nb:revAdjList[node]){
+        //         if(!visited[nb]){
+        //             q.push(nb);
+        //             visited[nb]=1;
+        //         }
+        //     }
+        // }
+        // return count;
+
+
+        // dfs 
+
         vector<int> visited(n,0);
-        // creating a adj List 
-        vector<vector<int>> adjList(n);
-        vector<vector<int>> revAdjList(n);
-        for(auto connection:connections){
-            adjList[connection[0]].push_back(connection[1]);
-            revAdjList[connection[1]].push_back(connection[0]);
+        vector<vector<pair<int,int>>> adjList(n);
+
+        for(auto &connection: connections){
+            adjList[connection[0]].push_back({connection[1],1});
+            adjList[connection[1]].push_back({connection[0],0});
         }
-
-        queue<int> q;
-        q.push(0);
-        visited[0]=1;
-
-        while(!q.empty()){
-
-            int node = q.front();
-            q.pop();
-
-            for(auto nb:adjList[node]){
-                if(!visited[nb]){
-                    count++;
-                    q.push(nb);
-                    visited[nb]=1;
-                }
-            }
-            for(auto nb:revAdjList[node]){
-                if(!visited[nb]){
-                    q.push(nb);
-                    visited[nb]=1;
-                }
-            }
-        }
+        dfsSolver(0,adjList,visited);
         return count;
         
     }
