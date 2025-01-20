7d74737ff29b439922d06e7d98326dfd827fe97c
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-01-20 21:14:05 +0530

    Time: 0 ms (100%), Space: 17.6 MB (46.96%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -38,9 +38,22 @@ public:
 
         // return min(solverRec(0,n,cost)+cost[0],cost[1]+solverRec(1,n,cost));
 
-        vector<int> dp(n+1,-1);
+        // vector<int> dp(n+1,-1);
 
-        return min(solverMem(0,n,cost,dp)+cost[0],cost[1]+solverMem(1,n,cost,dp));
+        // return min(solverMem(0,n,cost,dp)+cost[0],cost[1]+solverMem(1,n,cost,dp));
+
+        vector<int> dp(n+1,0);
+        dp[n-1]  = cost[n-1];
+        dp[n-2] = cost[n-2];
+
+        for(int i=n-3;i>=0;i--){
+            if(i+1<n && i+2 <n){
+                dp[i] = cost[i]+min(dp[i+1],dp[i+2]);
+            }
+        }
+
+        return min(dp[0],dp[1]);
+
         
     }
 };
