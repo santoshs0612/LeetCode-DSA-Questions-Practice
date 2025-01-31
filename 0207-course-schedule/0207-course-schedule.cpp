0b26cc883f510ec3427b7041370077d3ea9a6f58
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-01-31 19:25:50 +0530

    Time: 0 ms (100%), Space: 19.3 MB (76.66%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -20,6 +20,27 @@ public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
 
 
+        // DFS Solver
+
+        // vector<vector<int>> adj(numCourses);
+
+        // for(auto &it : prerequisites){
+        //     adj[it[1]].push_back(it[0]);
+        // }
+        // vector<int> visited(numCourses,0);
+        // vector<int> pathVisited(numCourses,0);
+
+        // for(int i=0;i<numCourses;i++){
+        //     if(!visited[i]){
+        //         if(dfSolverRec(i,adj,visited,pathVisited)==true){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+
+        // using khans algo BFS 
+
 
         vector<vector<int>> adj(numCourses);
 
@@ -27,17 +48,39 @@ public:
             adj[it[1]].push_back(it[0]);
         }
         vector<int> visited(numCourses,0);
-        vector<int> pathVisited(numCourses,0);
+        vector<int> indegree(numCourses,0);
+        for(auto &it : prerequisites){
+            indegree[it[0]]++;
+        }
+
+        queue<int> q;
 
         for(int i=0;i<numCourses;i++){
-            if(!visited[i]){
-                if(dfSolverRec(i,adj,visited,pathVisited)==true){
-                    return false;
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+
+        while(!q.empty()){
+
+            int node = q.front();
+            q.pop();
+
+            for(auto &it: adj[node]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
                 }
             }
         }
-        return true;
 
+
+        for(int i=0;i<numCourses;i++){
+            if(indegree[i]!=0){
+                return false;
+            }
+        }
+        return true;
 
         
     }
