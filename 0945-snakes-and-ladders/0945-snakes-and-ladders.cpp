033ab6e70b25417282937d41150552fd4d34902c
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-02-03 11:59:45 +0530

    Time: 4 ms (52.54%), Space: 16.9 MB (67.2%) - LeetHub

--- 0945-snakes-and-ladders/0945-snakes-and-ladders.cpp
+++ 0945-snakes-and-ladders/0945-snakes-and-ladders.cpp
@@ -38,11 +38,52 @@ public:
 
         int n = board.size();
 
-        vector<int> visited(n*n, INT_MAX);
+        // vector<int> visited(n*n, INT_MAX);
         int res = INT_MAX;
-        dfsSolverRec(0,0,res,board,visited);
+        // dfsSolverRec(0,0,res,board,visited);
 
-        return (res == INT_MAX)? -1 :res;
+        // return (res == INT_MAX)? -1 :res;
+
+        vector<int> visited(n*n+1, -1);
+        queue<pair<int,int>> q;
+        q.push({1,0});
+        visited[1] = 1;
+        while(!q.empty()){
+            
+            int pos = q.front().first;
+            int step = q.front().second;
+            q.pop();
+            if(pos == ((n*n))){
+                return min(visited[n*n],step);
+            }
+
+            for(int i=1;i<=6;i++){
+                int newPos = pos + i;
+                if(newPos > n*n){
+                    continue;
+                }
+
+                // boustrophedom grid conversion 
+                int row = (newPos -1)/n;
+                int col = (newPos-1)%n;
+                if(row%2==1){
+                    col = n-1-col;
+                }
+                row = n-1-row;
+
+                if(board[row][col]!=-1){
+                    newPos = board[row][col];
+                }
+                if(visited[newPos]==-1){
+                    q.push({newPos,step+1});
+                    visited[newPos]=step+1;
+                }
+                
+            }
+
+        }
+
+        return -1;
 
     }
 };
