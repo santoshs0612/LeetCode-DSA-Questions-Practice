aa92c7cf4f6fc3411ad65a48245c36f2a98cee28
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-02-18 23:14:58 +0530

    Time: 92 ms (80.98%), Space: 13.9 MB (56.88%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -44,12 +44,36 @@ public:
         // nums.insert(nums.begin(),1);
 
         // return solverRec(1,n,nums);
+        // int n = nums.size();
+        // nums.push_back(1);
+        // nums.insert(nums.begin(),1);
+
+        // vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+
+        // return solverMem(1,n,nums,dp);
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(),1);
 
-        vector<vector<int>> dp(n+2,vector<int>(n+2,-1));
+        vector<vector<int>> dp(n+2,vector<int>(n+2,0));
 
-        return solverMem(1,n,nums,dp);
+        for(int i= n;i>=1;i--){
+            for(int j=1;j<=n;j++){
+                if(i>j){
+                    continue;
+                }
+
+                int minBallon = INT_MIN;
+
+                for(int k=i;k<=j;k++){
+                    int val = nums[i-1]*nums[k]*nums[j+1]+ dp[i][k-1]+ dp[k+1][j];
+                    minBallon = max(minBallon,val);
+                }
+                dp[i][j]=minBallon;
+
+            }
+        }
+
+        return dp[1][n];
     }
 };
