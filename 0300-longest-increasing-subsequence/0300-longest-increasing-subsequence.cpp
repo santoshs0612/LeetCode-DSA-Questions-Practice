594aa0509e9cc5633acb62878952fad2e1461240
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-12-23 11:37:54 +0530

    Time: 564 ms (12.58%), Space: 296.7 MB (5.98%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,104 +1,56 @@
 class Solution {
-private:
-    int solverRec(int ind, int prev , vector<int> &arr){
-
-        if(ind==arr.size()){
-            return 0;
-        }
-
-        int notTake = solverRec(ind+1,prev,arr);
-        int take = 0;
-        
-        if(prev ==-1 || arr[ind]>arr[prev]){
-            take = solverRec(ind+1,ind,arr)+1;
-        }
-        return max(notTake, take);
-    }
-    int solverMem(int ind, int prev , vector<int> &arr,vector<vector<int>> &dp){
-
-        if(ind==arr.size()){
-            return 0;
-        }
-
-        if(dp[ind][prev+1]!=-1){
-            return dp[ind][prev+1];
-        }
-
-        int notTake = solverMem(ind+1,prev,arr,dp);
-        int take = 0;
-        
-        if(prev ==-1 || arr[ind]>arr[prev]){
-            take = solverMem(ind+1,ind,arr,dp)+1;
-        }
-        return dp[ind][prev+1]=max(notTake, take);
-    }
 public:
+
+    int solverRec(int index, int prev, vector<int> &nums){
+
+        //base case 
+
+        if(index>= nums.size()){
+            return 0;
+        }
+
+        // two cases take or not take
+
+        // first not take 
+        int notTake = solverRec(index+1, prev,nums);
+        int take = 0;
+        if(prev==-1 || nums[index] >nums[prev]){
+            take =  solverRec(index+1, index, nums )+1;
+        }
+        return max(take, notTake);
+    }
+    int solverMemorization(int index, int prev, vector<int> &nums, vector<vector<int>> &dp){
+
+        //base case 
+
+        if(index>= nums.size()){
+            return 0;
+        }
+
+        if(dp[index][prev+1]!=-1){
+            return dp[index][prev+1];
+        }
+
+        // two cases take or not take
+
+        // first not take 
+        int notTake = solverMemorization(index+1, prev,nums,dp);
+        int take = 0;
+        if(prev==-1 || nums[index] >nums[prev]){
+            take =  solverMemorization(index+1, index, nums,dp)+1;
+        }
+        return dp[index][prev+1]=max(take, notTake);
+    }
     int lengthOfLIS(vector<int>& nums) {
 
-        int n = nums.size();
+        // recurssive apporach 
         // return solverRec(0,-1,nums);
 
-        // vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        // Memorization 
+        int n = nums.size();
+        vector<vector<int>> dp(n+1, vector<int>(n+1,-1));
 
-        // return solverMem(0,-1,nums, dp);
+        return solverMemorization(0,-1,nums,dp);
 
-        // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
-
-
-        // for(int ind=n-1;ind>=0;ind--){
-
-        //     for(int prev = ind-1;prev>=-1;prev--){
-
-        //         int notTake = dp[ind+1][prev+1];
-        //         int take = 0;
-                
-        //         if(prev ==-1 || nums[ind]>nums[prev]){
-        //             take = dp[ind+1][ind+1]+1;
-        //         }
-        //         dp[ind][prev+1]=max(notTake, take);
-        //     }
-        // }
-        // return dp[0][0];
-
-
-        // vector<int> dp(n,1);
-
-        // for(int ind=0;ind<n;ind++){
-        //     for(int prevInd =0;prevInd<=ind-1;prevInd++){
-
-        //         if(nums[prevInd]<nums[ind]){
-        //             dp[ind] = max(dp[ind], 1+dp[prevInd]);
-        //         }
-        //     }
-        // }
-
-        // int maxLen = 0;
-
-        // for(int i=0;i<n;i++){
-
-        //     maxLen = max(maxLen, dp[i]);
-        // }
-
-        // return maxLen;
-
-
-        // binary search method  
-        int maxLen =1;
-        vector<int> temp;
-        temp.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-
-            if(nums[i]>temp.back()){
-                maxLen++;
-                temp.push_back(nums[i]);
-            }
-            else{
-                int ind = lower_bound(temp.begin(), temp.end(), nums[i])- temp.begin();
-                temp[ind] = nums[i]; 
-            }
-        }
-
-        return maxLen;
-        
     }
 };
