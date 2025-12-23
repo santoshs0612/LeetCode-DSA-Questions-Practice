bd33d638f2a438a253c22bc26490908583f775dc
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-12-23 19:40:11 +0530

    Time: 23 ms (80.59%), Space: 27.5 MB (51.02%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -46,9 +46,27 @@ public:
 
         // return solverRec(0,0, n,m,text1, text2);
 
-        vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
+        //vector<vector<int>> dp(n+1, vector<int> (m+1,-1));
 
-        return solverMem(0,0,n,m,text1,text2,dp);
+        // return solverMem(0,0,n,m,text1,text2,dp);
+
+
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+
+                if(text1[i]==text2[j]){
+                    dp[i][j]= 1+dp[i+1][j+1];
+                }
+                else{
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1]);
+                }
+            }
+        }
+
+        return dp[0][0];
+
         
     }
 };
