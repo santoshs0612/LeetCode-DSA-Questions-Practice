d26ed3cf17e419ea1930d5123a0cd13c042c8f39
Santosh Kumar <55349805+santoshs0612@users.noreply.github.com>
2025-12-25 21:52:48 +0530

    Time: 64 ms (10.26%), Space: 84.6 MB (5.18%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,71 +1,53 @@
 class Solution {
-private:
-    int solverRec(int n, int m , string str1, string str2){
-
-        // base case 
-        if(n<0){
-            return m+1;
-        }
-        if(m<0){
-            return n+1;
-        }
-
-        if(str1[n]==str2[m]){
-            return solverRec(n-1,m-1,str1,str2);
-        }
-        // if(str1[n]!=str2[n]){
-            return 1+min(solverRec(n,m-1,str1,str2),min(solverRec(n-1,m,str1,str2),solverRec(n-1,m-1,str1,str2)));
-        
-    }
-    int solverMem(int n, int m , string str1, string str2,vector<vector<int>> &dp){
-
-        // base case 
-        if(n<0){
-            return m+1;
-        }
-        if(m<0){
-            return n+1;
-        }
-        if(dp[n][m]!=-1){
-            return dp[n][m];
-        }
-        if(str1[n]==str2[m]){
-            return dp[n][m]=solverMem(n-1,m-1,str1,str2,dp);
-        }
-        // if(str1[n]!=str2[n]){
-            return dp[n][m]=1+min(solverMem(n,m-1,str1,str2,dp),min(solverMem(n-1,m,str1,str2,dp),solverMem(n-1,m-1,str1,str2,dp)));
-        
-    }
 public:
+    int solverRec(int i, int j, string word1, string word2){
+
+        // base case  
+        if(i<0){
+            return j+1;
+        }
+        if(j<0){
+            return i+1;
+        }
+
+        if(word1[i]==word2[j]){
+            return solverRec(i-1,j-1,word1,word2);
+        }else{
+            int deleteChar = solverRec(i-1,j,word1,word2);
+            int insert = solverRec(i,j-1,word1,word2);
+            int replace = solverRec(i-1,j-1,word1,word2);
+
+            return 1+ min(deleteChar, min(insert,replace));
+        }
+    }
+    int solverMem(int i, int j, string word1, string word2, vector<vector<int>>&dp){
+
+        // base case  
+        if(i<0){
+            return j+1;
+        }
+        if(j<0){
+            return i+1;
+        }
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        if(word1[i]==word2[j]){
+            return dp[i][j]=solverMem(i-1,j-1,word1,word2,dp);
+        }else{
+            int deleteChar = solverMem(i-1,j,word1,word2,dp);
+            int insert = solverMem(i,j-1,word1,word2,dp);
+            int replace = solverMem(i-1,j-1,word1,word2,dp);
+
+            return dp[i][j]=1+ min(deleteChar, min(insert,replace));
+        }
+    }
     int minDistance(string word1, string word2) {
-
-        int n= word1.length();
-        int m = word2.length();
-
+        int n = word1.size();
+        int m = word2.size();
         // return solverRec(n-1,m-1,word1,word2);
-
-        // memorization  
-        // vector<vector<int>> dp(n+1,vector<int> (m+1,-1));
-        // return solverMem(n-1,m-1,word1,word2,dp);
-        vector<vector<int>> dp(n+1,vector<int> (m+1,0));
-        for(int i=0;i<=n;i++){
-            dp[i][0]=i;
-        }
-        for(int i=0;i<=m;i++){
-            dp[0][i]=i;
-        }
-        for(int ind1= 1;ind1<=n;ind1++){
-            for(int ind2=1;ind2<=m;ind2++){
-                if(word1[ind1-1]==word2[ind2-1]){
-                    dp[ind1][ind2]=dp[ind1-1][ind2-1];
-                }
-                // if(str1[n]!=str2[n]){
-                else{
-                    dp[ind1][ind2]=1+min(dp[ind1][ind2-1],min(dp[ind1-1][ind2],dp[ind1-1][ind2-1]));
-                }
-                
-            }
-        }
-        return dp[n][m];
+        vector<vector<int>> dp(n+1, vector<int>(m+1,-1));
+        return solverMem(n-1,m-1,word1,word2,dp);
+        
     }
 };
